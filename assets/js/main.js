6c75442284913ec9119ad76fd31ea56c7abd6940
Johan Castillo <jcjohan2707@gmail.com>
2020-09-04 09:56:58 -0400

    Validacion de credenciales

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'App with Vue.js'
+    title: 'App with Vue.js',
+    name: "",
+    pass: ""
+  },
+  methods: {
+    validar(){
+
+      if(this.name == "johan" && this.pass == "1234"){
+        window.location.href = 'newPage.html'
+      }else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Datos incorrectos!',
+          footer: '<p>¿No tienes cuenta? <a href="register.html">Registrate.</a></p>'
+        })
+      }
+
+    }
   }
 });
