75bbb33060d94be981a08adadeed78069bb0b6c8
Johan Castillo <jcjohan2707@gmail.com>
2020-09-05 17:36:56 -0400

    Calculando el Down Payment

--- backend/calcular.php
+++ backend/calcular.php
@@ -1,12 +1,19 @@
 <?php
 
   if(isset($_POST["credito"])){
-    $credito = $_POST["credito"];
-    $anos = $_POST["anos"];
-    $intereses = $_POST["intereses"];
-    $downpayment = $_POST["downpayment"];
+    $credito = floatval($_POST["credito"]);
+    $anos = floatval($_POST["anos"]);
+    $intereses = floatval($_POST["intereses"]);
+    $downpayment = floatval($_POST['downpayment']);
     $totalin = 0;
 
+    //Calculos
+    $downpayment = $downpayment * $credito / 100;
+    $credito = $credito - $downpayment;
+
+
+
+    //echo $downpayment;
     echo $credito;
   }
 
