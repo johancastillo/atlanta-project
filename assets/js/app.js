03c06dcd19c990524c2501d4d20803781dd907d7
Johan Castillo <jcjohan2707@gmail.com>
2020-09-05 14:41:41 -0400

    Tabla dinamica

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,18 @@ const app = new Vue({
   data: {
     credito: "",
     anos: 15,
-    downpayment: 3,5,
-    intereses: ""
+    downpayment: 3.5,
+    intereses: "",
+    results: [
+      {mes: 1, intereses: 24, amortizacion: 23, cuota_mensual: 150, capital_pendiente: 20},
+      {mes: 2, intereses: 24, amortizacion: 23, cuota_mensual: 150, capital_pendiente: 20},
+      {mes: 3, intereses: 24, amortizacion: 23, cuota_mensual: 150, capital_pendiente: 20},
+
+    ]
   },
   methods: {
     calcular(){
-      alert(this.intereses);
+      alert(result);
     }
   }
 });
